0b4cd5400bcde8c391b0de413c06d5d7d06ace85
Shinichirow KAMITO <s-kamito@groovenauts.jp>
2022-08-01 17:20:06 +0900

    vite オプションなどを修正。

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,4 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,6 +7,12 @@ export default defineConfig({
   plugins: [
     react()
   ],
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: resolve(__dirname, "src/main.tsx")
+    }
+  },
   server: {
     host: "0.0.0.0",
     port: 3003
