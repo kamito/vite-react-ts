f923b2aefa6d68777c2319b0462908911b0bd99a
Shinichirow KAMITO <s-kamito@groovenauts.jp>
2022-08-01 17:18:49 +0900

    manifest を読み込んで読み込む。

--- app/app.py
+++ app/app.py
@@ -16,13 +16,16 @@ app.permanent_session_lifetime = datetime.timedelta(hours=24)
 IS_DEBUG = app.config["DEBUG"]
 
 WHITENOISE_MAX_AGE = 31536000 if not IS_DEBUG else 0
+whitenoize_base_path = "../frontend" if IS_DEBUG else "../frontend/dist"
+whitenoise_root_path = os.path.abspath(os.path.join(
+    os.path.dirname(__file__), whitenoize_base_path))
 app.wsgi_app = WhiteNoise(
     app.wsgi_app,
-    root=os.path.join(os.path.dirname(__file__), "../frontend"),
+    root=whitenoise_root_path,
     prefix="",
     max_age=WHITENOISE_MAX_AGE,
     autorefresh=IS_DEBUG
     )
 
-app.jinja_env.globals.update(Const.__dict__)
-app.jinja_env.globals.update({"IS_DEBUG": IS_DEBUG})
+const_o = Const(is_debug=IS_DEBUG)
+app.jinja_env.globals.update(const_o.__dict__)
--- app/const.py
+++ app/const.py
@@ -1,4 +1,5 @@
 import os
+import json
 from dotenv import load_dotenv
 
 load_dotenv(override=True)
@@ -10,3 +11,32 @@ IS_LOCAL = os.getenv('IS_LOCAL', False)
 class Const(object):
 
     IS_LOCAL = IS_LOCAL
+
+
+    def __init__(self, is_debug=False):
+        self.IS_DEBUG = is_debug
+        self.IS_LOCAL = self.__class__.IS_LOCAL
+
+        self.JAVASCRIPTS = []
+        self.STYLESHEETS = []
+        self.load_manifest()
+
+    def load_manifest(self):
+        if not self.IS_DEBUG:
+            manifest = None
+            manifest_path = os.path.abspath(
+                os.path.join(
+                    os.path.dirname(__file__),
+                    "../frontend/dist/manifest.json"
+                    ))
+            with open(manifest_path) as fp:
+                manifest = json.load(fp)
+
+            if manifest:
+                for k, m in manifest.items():
+                    filename = m['file']
+                    ext = filename.split('.').pop()
+                    if ext == "js":
+                        self.JAVASCRIPTS.append(filename)
+                    elif ext == "css":
+                        self.STYLESHEETS.append(filename)
