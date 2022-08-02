9141622bf9be3249e1edf045da4877d2e193266c
Shinichirow KAMITO <s-kamito@groovenauts.jp>
2022-08-02 17:02:49 +0900

    blocks_login デコレータを追加。

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import request, session, render_template, redirect, jsonify
 from .app import app
+from .blocks import blocks, blocks_login
 
 @app.route("/")
+@blocks_login
 def top():
+    data = blocks.login_data
     return render_template("top.html")
